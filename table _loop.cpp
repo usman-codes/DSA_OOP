31fe02ac91b6b7ba4d3412c04af21533b90cb455
usman-codes <mianusmanamin89425@gmail.com>
2025-01-13 18:06:06 +0500

    intial commit

--- table _loop.cpp
+++ table _loop.cpp
@@ -3,12 +3,36 @@
 using namespace std;
 
 int main() {
-    //Table of given no upto 10 using for loop
+        //Table of given no upto 10 using for loop
+    
+    // int no;
+    // cout<<"Enter the table number = ",cin>>no;
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     cout<< no  << "*"<< i <<" "<<"="<< no*i<<endl;
+    // }
+
+        //Table of given no upto 10 using While loop 
+    //  int no;
+    //  cout<<"Enter the table number = ",cin>>no;
+    //  int i=1;
+    //  while (i<=10)
+    //  {
+        // cout<< no  << "*"<< i <<" "<<"="<< no*i<<endl;
+        // i++;
+
+    //  }
+
+         //Table of given no upto 10 using While loop 
     int no;
-    cout<<"Enter the table number = ",cin>>no;
-    for (int i = 1; i <= 10; i++)
+     cout<<"Enter the table number = ",cin>>no;
+    int i=1;
+    do
     {
         cout<< no  << "*"<< i <<" "<<"="<< no*i<<endl;
-    }
+        i++;
+    } while (i<=10);
+    
+
      
 }
