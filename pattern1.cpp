039951e5b6e92abb26127d4e9149b0dc7217e5d4
usman-codes <mianusmanamin89425@gmail.com>
2025-01-13 19:00:32 +0500

    intial commit

---
+++ pattern1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+     int no;
+     cout<<"Enter the  number = ",cin>>no;
+     for (int i = 1; i <= no; i++)
+     {
+        for (int j = 1; j <= no; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        
+     }
+     
+}
